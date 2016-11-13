adeaeb18d46c85d9a7bdb3ffd0c3122452e1feb7
Shilo Denis <shilo.denis.1997g@gmail.com>
2016-11-13 20:10:42 +0300

    Abstract Shape class created

---
+++ Task2.Logic/Shape.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2.Logic
+{
+    public abstract class Shape
+    {
+        public abstract double Perimeter { get; protected set; }
+        public abstract double Square { get; protected set; }
+    }
+}
